2711d1b4bec6ebf4c3c5483cb458932cb3dcd800
leepood <leepood@gmail.com>
2015-06-01 13:07:29 +0800

    first init

---
+++ gen/panda/safari/island/pop/bubble/shooter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package panda.safari.island.pop.bubble.shooter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
